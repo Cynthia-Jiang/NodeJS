4e6adeba2413c7e7683dde86ea36603cf75789bd
Cynthia-Jiang <yayago_up@outlook.com>
2016-01-25 15:01:51 +0800

    update 1/25/2016

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ var url = require("url");
 
 function start(route,handle) {
     function onRequest(request, response) {
+	var postdata = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname +" received.");
 
-        /*var content=*/route(handle,pathname,response);
+	request.setEncoding("utf8");
+
+	request.addListener("data",function(postDataChunk){
+	   postData +=postDataChunk;
+	   console.log("Received POST data chunk '"+ postDataChunk + "'.");
+     });
+
+     request.addListener("end",function(){
+	route(handle,pathname,response,postData);
+     });
+
+        /*var content=route(handle,pathname,response);*/
 /*
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write(content);
